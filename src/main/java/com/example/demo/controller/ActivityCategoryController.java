ec2e131fc5e75c28024588ccf227b797077079c9
bala-amypo <bala@amypo.in>
2025-12-16 23:54:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/ActivityCategoryController.java
+++ src/main/java/com/example/demo/controller/ActivityCategoryController.java
@@ -1,4 +1,4 @@
 package com.example.demo.controller;
-public class ActivityCatgoryController{
+public class ActivityCategoryController{
     
 }
--- src/main/java/com/example/demo/repository/ActivityLogRepository.java
+++ src/main/java/com/example/demo/repository/ActivityLogRepository.java
@@ -1,4 +1,4 @@
 package com.example.demo.repository;
-public class ActivityLogRespository{
+public class ActivityLogRepository{
     
 }
