afc63b7d04fa830d92a90ead4781af8b342b63e4
bala-amypo <bala@amypo.in>
2025-12-18 23:45:41 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.example.demo.security;
+/* package com.example.demo.security;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
@@ -27,4 +27,4 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .build();
     }
 
-}
+} */
