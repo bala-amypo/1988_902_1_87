3d429e7173475408798c249f38c071797a71941b
bala-amypo <bala@amypo.in>
2025-12-23 16:18:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 @Tag(name = "Authentication", description = "User authentication operations")
 public class AuthController {
